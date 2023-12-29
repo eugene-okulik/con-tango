0b474179b2f003969ae3e6e3f5a8b8a68d57841e
Dmytro Shylko <stanedimon@yahoo.com>
2023-12-29 09:06:42 +0200

    homework_4 done

--- homework/dmytro_shylko/homework_4/task.py
+++ homework/dmytro_shylko/homework_4/task.py
@@ -1,14 +1,11 @@
-my_dict = {"tuple": (1, 2, 3, 4, 5),
-            "list": [10, 20, 30, 40, 50],
-            "dict": {
-                1: "apple",
-                2: "pea",
-                3: "plum",
-                4: "melon",
-                5: "grape"
-                },
-            "set": {100, 200, 300, 400, 500}
-            }
+my_dict = {
+    "tuple": (1, 2, 3, 4, 5),
+    "list": [10, 20, 30, 40, 50],
+    "dict": {1: "apple", 2: "pea", 3: "plum", 4: "melon", 5: "grape"},
+    "set": {100, 200, 300, 400, 500},
+}
+
+print(my_dict["tuple"][-1])
 
 my_dict["list"].append(60)
 my_dict["list"].pop(1)
