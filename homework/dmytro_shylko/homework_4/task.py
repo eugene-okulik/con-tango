969b2f49852fc371be3504c9e41c9e052c9fadc1
Dmytro Shylko <stanedimon@yahoo.com>
2023-12-30 18:30:06 +0200

    homework_4 done corrected

--- homework/dmytro_shylko/homework_4/task.py
+++ homework/dmytro_shylko/homework_4/task.py
@@ -10,7 +10,7 @@ print(my_dict["tuple"][-1])
 my_dict["list"].append(60)
 my_dict["list"].pop(1)
 
-my_dict["dict"]["i am a tuple"] = ("orange", "apricot")
+my_dict["dict"]['i am a tuple',] = ("orange", "apricot")
 my_dict["dict"].pop(3)
 
 my_dict["set"].add(444)
