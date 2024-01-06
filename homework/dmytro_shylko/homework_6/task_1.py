227d4115c71fd7f6ad62db1fdd6ad9db772bccc7
Dmytro Shylko <stanedimon@yahoo.com>
2024-01-06 11:48:55 +0200

    homework_6 done pylint_2

--- homework/dmytro_shylko/homework_6/task_1.py
+++ homework/dmytro_shylko/homework_6/task_1.py
@@ -1,5 +1,6 @@
 # pylint: disable=trailing-whitespace
-text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel.\
+Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 
 words = text.split()
 new_text = []
