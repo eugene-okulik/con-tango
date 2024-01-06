28a5365f21d044600868cb8512a9a29dccc02b61
Dmytro Shylko <stanedimon@yahoo.com>
2024-01-06 11:39:31 +0200

    homework_6 done

---
+++ homework/dmytro_shylko/homework_6/task_1.py
@@ -0,0 +1,18 @@
+# pylint: disable=trailing-whitespace
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+words = text.split()
+new_text = []
+
+for word in words:
+    if "," in word:
+        word = word.replace(",", "ing,")
+        new_text.append(word)
+    elif "." in word:
+        word = word.replace(".", "ing.")
+        new_text.append(word)
+    else:
+        word += "ing"
+        new_text.append(word)
+
+print(" ".join(new_text))
