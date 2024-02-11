17adf97c7efd27cf4c546d9e2b70126c7fc1d2cd
Dmytro Shylko <stanedimon@yahoo.com>
2024-02-11 12:43:16 +0200

    dmytro_shylko homework_9 pylint_3

--- homework/dmytro_shylko/homework_9/task_2.py
+++ homework/dmytro_shylko/homework_9/task_2.py
@@ -1,8 +1,37 @@
 # pylint: disable=missing-module-docstring
 
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32,
-                 34, 30, 29, 25, 27, 22, 22, 23, 25, 29,
-                    29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+temperatures = [
+    20,
+    15,
+    32,
+    34,
+    21,
+    19,
+    25,
+    27,
+    30,
+    32,
+    34,
+    30,
+    29,
+    25,
+    27,
+    22,
+    22,
+    23,
+    25,
+    29,
+    29,
+    31,
+    33,
+    31,
+    30,
+    32,
+    30,
+    28,
+    24,
+    23,
+]
 
 hot_temperatures = filter(lambda x: x > 28, temperatures)
 
@@ -11,4 +40,4 @@ hots_list = list(hot_temperatures)
 
 print(max(hots_list))
 print(min(hots_list))
-print(round(sum(hots_list)/len(hots_list)))
+print(round(sum(hots_list) / len(hots_list)))
